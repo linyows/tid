71858e22b2b57067908aa5297f946c1dd7077520
linyows <linyows@gmail.com>
2015-02-17 11:56:47 +0900

    add spec

---
+++ spec/tid_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe 'Tid' do
+  describe 'ssh to docker container' do
+    let(:ssh_to_docker_container) do
+      [
+        "ssh root@#{ENV['TID_HOSTNAME']}",
+        "-p #{ENV['TID_PORT']}",
+        "-i #{ENV['TID_BASE_PATH']}/id_rsa",
+        "'echo yo'"
+      ].join(' ')
+    end
+
+    it 'successful' do
+      _, _, ex = cmd ssh_to_docker_container
+      expect(ex.exitstatus).to eq 0
+    end
+
+    it 'puts yo' do
+      out, _, _ = cmd ssh_to_docker_container
+      expect(out).to eq "yo\n"
+    end
+  end
+end
