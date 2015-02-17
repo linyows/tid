1b97e75a9e3610c0992db52c5b1a38bdaa7596d4
linyows <linyows@gmail.com>
2015-02-17 15:25:57 +0900

    add test

--- spec/tid_spec.rb
+++ spec/tid_spec.rb
@@ -1,6 +1,9 @@
 RSpec.describe 'Tid' do
   describe 'ssh to docker container' do
     let(:ssh_to_docker_container) do
+      puts `pwd`
+      puts `ls -las`
+      puts `ls -las #{ENV['TID_BASE_PATH']}/`
       [
         "ssh root@#{ENV['TID_HOSTNAME']}",
         "-p #{ENV['TID_PORT']}",
